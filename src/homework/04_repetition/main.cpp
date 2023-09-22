0d720d6aa455cd89b3cafe5c3209a09a7eb18416
SidneyCfur <143145277+SidneyCfur@users.noreply.github.com>
2023-09-22 04:45:04 +0000

    commit

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,36 @@
 //write include statements
-
+#include <iostream>
+#include "repetition.h"
 //write using statements
+using std::cout; using std::cin;
 
 int main() 
 {
+	auto choice1 = 0;
+	auto choice2 = 0;
+	auto exit = 'n';
+	do
+	{
+		cout<<"1- Factorial \n 2- Greatest Common Divisor \n 3- Exit";
+		cin>>choice1;
+		switch(choice1){
+		case 1:
+		cout<<"Enter one number for factorial.\n";
+		cin>>choice1;
+		cout<<factorial(choice1);
+		break;
+		case 2:
+		cout<<"Enter two numbers to compare fore greatest common divisor.\n";
+		cin>>choice1;
+		cin>>choice2;
+		cout<<gcd(choice1,choice2);
+		break;
+		case 3:
+		cout<<"Are you sure you want to exit? (y/n)\n";
+		cin>>exit;
+		break;
+		}
+	}
+	while(exit != 'n');
 	return 0;
 }
