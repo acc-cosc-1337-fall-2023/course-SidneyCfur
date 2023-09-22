f43e8132e78c54807b31c7725c920269ccc8f33a
SidneyCfur <143145277+SidneyCfur@users.noreply.github.com>
2023-09-22 04:18:02 +0000

    commit

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,29 @@
 //add include statements
 
 //add function(s) code here
+
+int factorial(int num)
+{ 
+auto result = 1;
+while(num > 0) {
+    result = result * num;
+    num--;
+    }
+return result;
+}
+
+int gcd(int num1, int num2)
+{
+auto holder = 0;
+while(num1 != num2){
+    if(num1 < num2){
+        holder = num1;
+        num1 = num2;
+        num2 = holder;
+    }
+    if(num1 > num2){
+        num1 -= num2;
+    }
+    return num1;
+    }
+}
