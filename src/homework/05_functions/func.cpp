f285522bbd45af016b26fe1dc79b01c49ef3c363
SidneyCfur <143145277+SidneyCfur@users.noreply.github.com>
2023-09-29 22:44:07 +0000

    commit

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,26 @@
 //add include statements
-
+#include<string>
 //add function code here
+using std::string;
+
+double get_gc_content(const string& dna){
+double output = 0.0;
+double counter;
+string list = dna;
+for(auto i=0; i < 8; i++){
+    if(list[i] =='g' || list[i] == 'a'){
+        counter += 1.0;
+    }
+    counter /= 8.0;
+}
+
+return output;
+}
+
+string get_dna_compelemt(string dna){
+
+}
+
+string reverse_string(string dna){
+
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,9 @@
+#include<string>
 //add function(s) prototype here
+using std::string;
+
+double get_gc_content(const string& dna);
+
+string get_dna_compelemt(string dna);
+
+string reverse_string(string dna);
