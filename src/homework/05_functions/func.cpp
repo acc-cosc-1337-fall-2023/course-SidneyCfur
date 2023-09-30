6f6a7817ae8cf1ff718ac94b5949afa2a5cb7ffb
SidneyCfur <143145277+SidneyCfur@users.noreply.github.com>
2023-09-30 17:37:03 +0000

    commit

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,26 +1,51 @@
 //add include statements
 #include<string>
+#include<iostream>
 //add function code here
-using std::string;
+using std::string; using std::cout;
 
 double get_gc_content(const string& dna){
-double output = 0.0;
-double counter;
-string list = dna;
-for(auto i=0; i < 8; i++){
-    if(list[i] =='g' || list[i] == 'a'){
-        counter += 1.0;
+
+    double output = 0.0;
+    string list = dna;
+
+    for(auto i=0; i < dna.length(); i++){
+        if(list[i] =='G' || list[i] == 'C'){
+            output += 1.0;
     }
-    counter /= 8.0;
+    
+}
+    output /= 8.0;
+    return output;
 }
 
+
+string get_reverse_string(string dna) {
+    string output = dna;
+    int count = 0;
+    for(int i = dna.length() - 1; i >= 0; i--){
+        output[count] = dna[i];
+        count++;
+}
 return output;
 }
 
-string get_dna_compelemt(string dna){
+string get_dna_complement(string dna){
+    string output = get_reverse_string(dna);
 
-}
-
-string reverse_string(string dna){
-
+    for(int i = 0; i <= output.length(); i++){
+        if(output[i] == 'A'){
+            output[i] = 'T';
+        }
+        else if(output[i] == 'T'){
+            output[i] = 'A';
+        }
+        else if(output[i] == 'G'){
+            output[i] = 'C';
+        }
+        else if(output[i] == 'C'){
+            output[i] = 'G';
+        }
+    }
+    return output;
 }
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,9 +1,10 @@
 #include<string>
+#include<iostream>
 //add function(s) prototype here
-using std::string;
+using std::string; using std::cout;
 
 double get_gc_content(const string& dna);
 
-string get_dna_compelemt(string dna);
+string get_dna_complement(string dna);
 
-string reverse_string(string dna);
+string get_reverse_string(string dna);
