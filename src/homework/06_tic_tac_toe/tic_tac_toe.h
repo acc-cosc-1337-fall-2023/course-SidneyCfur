de9d9aef7a86fee791bcdd1dc557bbff1076f8ef
SidneyCfur <143145277+SidneyCfur@users.noreply.github.com>
2023-10-06 23:14:49 +0000

    commit

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,25 @@
 //h
+#include<iostream>
+#include<string>
+#include<vector>
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(std::string first_player);
+    void mark_board(int position);
+    std::string get_player() const{return player;}
+    void display_board() const;
+private:
+    void clear_board();
+    void set_next_player();
+    std::string player;
+    std::vector<std::string> pegs{9, " "};
+    bool check_board_full();
+};
+
+
+#endif
